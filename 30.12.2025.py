3f421c39bce43012648506b3a8b2a18975a6e3a6
appigoo <appigoodoc@gmail.com>
2026-01-02 11:49:04 +0000

    Update 30.12.2025.py

--- 30.12.2025.py
+++ 30.12.2025.py
@@ -328,6 +328,7 @@ CONTINUOUS_UP_THRESHOLD = st.number_input("連續上漲閾值 (根K線)", min_va
 CONTINUOUS_DOWN_THRESHOLD = st.number_input("連續下跌閾值 (根K線)", min_value=1, max_value=20, value=3, step=1)
 PERCENTILE_THRESHOLD = st.selectbox("選擇 Price Change %、Volume Change %、Volume、股價漲跌幅 (%)、成交量變動幅 (%) 數據範圍 (%)", percentile_options, index=1)
 REFRESH_INTERVAL = st.selectbox("选择刷新间隔 (秒)", refresh_options, index=refresh_options.index(300))
+time.sleep(2)
 #
 all_signal_types = [
     "📉 High<Low", "📉 MACD賣出", "📉 EMA賣出", "📉 價格趨勢賣出", "📉 價格趨勢賣出(量)", 
